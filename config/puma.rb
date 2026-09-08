cc21596368fc9db8dd52f9ab926c36839e376642
massanengineer <matsuda4645@gmail.com>
2026-09-08 17:47:19 +0900

    puma編集

--- config/puma.rb
+++ config/puma.rb
@@ -33,6 +33,7 @@ port ENV.fetch("PORT", 3000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+workers 0
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments.
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
